15e1d69b0b3d332b2e0412f895d16dda824c383d
Sergey <Sergeev_sergey99@mail.ru>
2022-07-13 19:33:27 +0300

    init

---
+++ Assets/Block.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public int value;
+    public int X;
+    public int Y;
+    private GameController _gc;
+
+    public void init(int v, int x, int y, GameController gc)
+    {
+        value = v;
+        X = x;
+        Y = y;
+        _gc = gc;
+        switch (v)
+        {
+            case -1: GetComponent<Image>().color = Color.gray;
+                break;
+            case 1: GetComponent<Image>().color = Color.red;
+                break;
+            case 2: GetComponent<Image>().color = Color.yellow;
+                break;
+            case 3: GetComponent<Image>().color = new Color(239/256f,114/256f,21/256f);
+                break;
+        }
+    }
+
+    private float range = 100;
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (value <= 0) return;
+        startDraged = false;
+    }
+
+    private bool isVertical;
+    private bool startDraged;
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+        if (value <= 0) return;
+        if (!startDraged)
+        {
+            if (Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y)) isVertical = false;
+            else isVertical = true;
+            startDraged = true;
+        }
+
+        if (transform.localPosition.y + eventData.delta.y < 0 && !(Y < 4 && _gc.isCellAvailable(X, Y + 1))) return;
+        if (transform.localPosition.y + eventData.delta.y > 0 && !(Y > 0 && _gc.isCellAvailable(X, Y - 1))) return;
+        if (transform.localPosition.x + eventData.delta.x < 0 && !(X > 0 && _gc.isCellAvailable(X - 1, Y))) return;
+        if (transform.localPosition.x + eventData.delta.x > 0 && !(X < 4 && _gc.isCellAvailable(X + 1, Y))) return;
+        
+
+        if(((Vector2)transform.localPosition + eventData.delta).magnitude < range) 
+            transform.localPosition += new Vector3(isVertical? 0: eventData.delta.x, isVertical? eventData.delta.y:0,0);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (value <= 0) return;
+        if(transform.localPosition.magnitude < range/4) transform.localPosition = Vector3.zero;
+        else
+        {
+            if(transform.localPosition.x > 0) updateCell(X+1,Y);
+            else if(transform.localPosition.x < 0) updateCell(X - 1, Y);
+            else if(transform.localPosition.y > 0) updateCell(X, Y - 1);
+            else if(transform.localPosition.y < 0) updateCell(X, Y + 1);
+            _gc.panel.SetActive(true);
+            StartCoroutine(MoveToZero());
+        }
+    }
+
+    void updateCell(int x, int y)
+    {
+        X = x;
+        Y = y;
+        transform.SetParent(_gc.GetCell(X, Y).transform);
+    }
+
+    IEnumerator MoveToZero()
+    {
+        var t = new WaitForSeconds(0.01f);
+
+        var EPS = 0.1f;
+        while (transform.localPosition.magnitude > EPS)
+        {
+            yield return t;
+            transform.localPosition = Vector3.Lerp(transform.localPosition, Vector3.zero, 0.3f);
+        }
+
+        transform.localPosition = Vector3.zero;
+        
+        _gc.panel.SetActive(false);
+        _gc.CheckField();
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using Random = UnityEngine.Random;
+
+public class GameController : MonoBehaviour
+{
+    private GameObject[,] field = new GameObject[5, 5];
+
+    public GameObject prefab;
+    public GameObject panel;
+    public GameObject winPanel;
+
+    private void Awake()
+    {
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                field[x, y] = transform.GetChild(x + y * 5).gameObject;
+            }
+        }
+
+        GenerateField();
+    }
+
+    public bool isCellAvailable(int x, int y)
+    {
+        return field[x, y].transform.childCount == 0;
+    }
+
+    public GameObject GetCell(int x, int y)
+    {
+        return field[x, y];
+    }
+
+    void SetBlock(int x, int y, int v)
+    {
+        Instantiate(prefab, field[x, y].transform).GetComponent<Block>().init(v, x, y, this);
+    }
+
+    bool isFieldWin()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                if (field[i * 2, y].transform.childCount != 1 ||
+                    field[i * 2, y].transform.GetChild(0).GetComponent<Block>().value != i + 1)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public void CheckField()
+    {
+        if(isFieldWin()) winPanel.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void ReGenerateField()
+    {
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                if (field[x, y].transform.childCount != 0) Destroy(field[x, y].transform.GetChild(0).gameObject);
+            }
+        }
+
+        StartCoroutine(NextFrame());
+
+    }
+
+    IEnumerator NextFrame()
+    {
+        yield return new WaitForFixedUpdate();
+        GenerateField();
+    }
+    
+
+    void GenerateField()
+    {
+        SetBlock(1, 0, -1);
+        SetBlock(3, 0, -1);
+        SetBlock(1, 2, -1);
+        SetBlock(3, 2, -1);
+        SetBlock(1, 4, -1);
+        SetBlock(3, 4, -1);
+
+        for (int j = 1; j <= 3; j++)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int x = Random.Range(0, 5);
+                int y = Random.Range(0, 5);
+                if (field[x, y].transform.childCount == 0) SetBlock(x, y, j);
+                else i--;
+            }
+        }
+        if(isFieldWin()) ReGenerateField();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
