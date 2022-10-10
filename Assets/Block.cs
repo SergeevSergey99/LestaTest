dc7260d85f8592fbdc878d97a3c185ceb1bcf452
Sergey <Sergeev_sergey99@mail.ru>
2022-10-11 02:37:01 +0300

    update angle

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,75 +20,95 @@ public class Block : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHan
         _gc = gc;
         switch (v)
         {
-            case -1: GetComponent<Image>().color = Color.gray;
+            case -1:
+                GetComponent<Image>().color = Color.gray;
                 break;
-            case 1: GetComponent<Image>().color = Color.red;
+            case 1:
+                GetComponent<Image>().color = Color.red;
                 break;
-            case 2: GetComponent<Image>().color = Color.yellow;
+            case 2:
+                GetComponent<Image>().color = Color.yellow;
                 break;
-            case 3: GetComponent<Image>().color = new Color(239/256f,114/256f,21/256f);
+            case 3:
+                GetComponent<Image>().color = new Color(239 / 256f, 114 / 256f, 21 / 256f);
                 break;
         }
     }
 
-    private float range = 100;
-    
+    private float range = 160;
+
+    private bool isVertical;
+    private bool setDraged;
+    private float EPS = 0.03f;
     public void OnBeginDrag(PointerEventData eventData)
     {
         if (value <= 0) return;
-        startDraged = false;
+        setDraged = false;
     }
-
-    private bool isVertical;
-    private bool startDraged;
     public void OnDrag(PointerEventData eventData)
     {
-        
         if (value <= 0) return;
-        if (!startDraged)
+        if (!setDraged || transform.localPosition.magnitude < range / 80)
         {
-            if (Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y)) isVertical = false;
-            else isVertical = true;
-            startDraged = true;
+            if (Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y))
+            {
+                isVertical = false;
+                transform.localPosition = new Vector3(transform.localPosition.x, 0, transform.localPosition.z);
+            }
+            else
+            {
+                isVertical = true;
+                transform.localPosition = new Vector3(0, transform.localPosition.y, transform.localPosition.z);
+            }
+
+            setDraged = true;
         }
 
         if (transform.localPosition.y + eventData.delta.y < 0 && !(Y < 4 && _gc.isCellAvailable(X, Y + 1))) return;
         if (transform.localPosition.y + eventData.delta.y > 0 && !(Y > 0 && _gc.isCellAvailable(X, Y - 1))) return;
         if (transform.localPosition.x + eventData.delta.x < 0 && !(X > 0 && _gc.isCellAvailable(X - 1, Y))) return;
         if (transform.localPosition.x + eventData.delta.x > 0 && !(X < 4 && _gc.isCellAvailable(X + 1, Y))) return;
-        
 
-        if(((Vector2)transform.localPosition + eventData.delta).magnitude < range) 
-            transform.localPosition += new Vector3(isVertical? 0: eventData.delta.x, isVertical? eventData.delta.y:0,0);
+
+        if (((Vector2) transform.localPosition + eventData.delta).magnitude < range)
+            transform.localPosition += new Vector3(
+                isVertical ? 0 : eventData.delta.x,
+                isVertical ? eventData.delta.y : 0,
+                0);
+
+        if (transform.localPosition.magnitude > (range / 8 * 7)) CheckAndUpdatePos();
     }
-
     public void OnEndDrag(PointerEventData eventData)
     {
         if (value <= 0) return;
-        if(transform.localPosition.magnitude < range/8) transform.localPosition = Vector3.zero;
-        else
+        if(!(transform.localPosition.magnitude < range / 8)) 
         {
-            if(transform.localPosition.x > 0) updateCell(X+1,Y);
-            else if(transform.localPosition.x < 0) updateCell(X - 1, Y);
-            else if(transform.localPosition.y > 0) updateCell(X, Y - 1);
-            else if(transform.localPosition.y < 0) updateCell(X, Y + 1);
+            CheckAndUpdatePos();
             _gc.panel.SetActive(true);
-            StartCoroutine(MoveToZero());
         }
+        StartCoroutine(MoveToZero());
     }
 
+    void CheckAndUpdatePos()
+    {
+        if (transform.localPosition.x > 0) updateCell(X + 1, Y);
+        else if (transform.localPosition.x < 0) updateCell(X - 1, Y);
+        else if (transform.localPosition.y > 0) updateCell(X, Y - 1);
+        else if (transform.localPosition.y < 0) updateCell(X, Y + 1);
+        
+    }
     void updateCell(int x, int y)
     {
         X = x;
         Y = y;
         transform.SetParent(_gc.GetCell(X, Y).transform);
+        setDraged = false;
     }
 
     IEnumerator MoveToZero()
     {
         var t = new WaitForSeconds(0.01f);
 
-        var EPS = 0.1f;
         while (transform.localPosition.magnitude > EPS)
         {
             yield return t;
@@ -95,7 +116,7 @@ public class Block : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHan
         }
 
         transform.localPosition = Vector3.zero;
-        
+
         _gc.panel.SetActive(false);
         _gc.CheckField();
     }
